c9153f6e85eada299372071de78b2bfeff1b0cfc
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-03-26 11:54:14 +0600

    laba 6

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
-// Require packages and set the port
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from "express";
+import bodyParser from "./node_modules/body-parser/index.js";
+import fetch from "node-fetch";
 
-const hostname = '127.0.0.1';
-const port = 8080;
+import {
+  fileURLToPath
+} from 'url';
+import {
+  dirname
+} from 'path';
+
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = dirname(__filename);
+
+const host = '127.0.0.1';
+const port = 3000;
 const app = express();
- 
+
 // Use Node.js body parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 
 app.use(express.static(__dirname));
 
 const urlencodedParser = bodyParser.urlencoded({
-    extended: false,
-  })
-
-  app.post('/message', urlencodedParser, function (
-    request,
-    response
-  ) {
-    if (!request.body) return response.sendStatus(400)
-    console.log(request.body)
-    response.send(
-      `Имя: ${request.body.Name} <br/>
-       Номер телефона: ${request.body.Phone} <br/>
-       Enail: ${request.body.Email} <br/>
-       Сообщение: ${request.body.Message} <br/>`
-    )
-  })
- 
-// Start the server
-const server = app.listen(port, hostname, (error) => {
-    if (error) return console.log(`Error: ${error}`);
-    console.log(`Server running at http://${hostname}:${server.address().port}`);
-    console.log(`Server listening on port ${server.address().port}`);
+  extended: false,
 });
 
+app.post('/message', urlencodedParser, function (
+  request,
+  response
+) {
+  if (!request.body) return response.sendStatus(400)
+  const User = {
+    'Имя': request.body.Name,
+    'Номер телефона': request.body.Phone,
+    'Email': request.body.Email,
+    'Сообщение': request.body.Message
+  }
+  response.send(User);
+});
+
+// Start the server
+const server = app.listen(port, host, (error) => {
+  if (error) return console.log(`Error: ${error}`);
+});
