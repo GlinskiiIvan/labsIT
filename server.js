b833898d5938ad18f6190132a3790e6fd7f0db3f
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-03-26 18:25:46 +0600

    laba 7

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import {
 import {
   dirname
 } from 'path';
+import { url } from "inspector";
 
 const __filename = fileURLToPath(
   import.meta.url);
@@ -27,7 +28,7 @@ app.use(express.static(__dirname));
 
 const urlencodedParser = bodyParser.urlencoded({
   extended: false,
-});
+})
 
 app.post('/message', urlencodedParser, function (
   request,
@@ -41,9 +42,38 @@ app.post('/message', urlencodedParser, function (
     'Сообщение': request.body.Message
   }
   response.send(User);
+})
+
+function auth(req, res, next) {
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401; 
+    next(err);
+  }
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); 
+  var username = auth[0];
+  var password = auth[1];
+  if (username == 'Ivan' && password == '123456') {
+    next();
+  } else {
+    var err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status  = 401; 
+    next(err);
+  }
+}
+app.use(auth);
+app.get('/secret', (req, res) => {
+  res.statusCode = 200;
+  res.redirect('./secret.html');
 });
+
+
 
 // Start the server
 const server = app.listen(port, host, (error) => {
   if (error) return console.log(`Error: ${error}`);
+  console.log(`Server listening on port ${server.address().port}`);
 });
